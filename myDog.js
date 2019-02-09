4fc860838248e8f5ec954eab7e62788b38c35f2c
Toomas Joamets <toomas.joamets@outlook.com>
2019-02-09 15:14:06 +0200

    A lot of changes

--- myDog.js
+++ myDog.js
@@ -1,7 +1,16 @@
-function Dog(name, age, breed) {
+const readline = require('readline');
+const rl = readline.createInterface(
+  {
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '\x1b[36mCOMMAND>\x1b[0m '
+  }
+);
+
+function Dog(name, breed, age) {
   this.dogName = name;
   this.dogAge = age;
-  this.breed = breed;
+  this.dogBreed = breed;
 
   this.getName = function () {
     return this.dogName;
@@ -11,49 +20,107 @@ function Dog(name, age, breed) {
     return this.dogAge;
   }
 
+  this.getBreed = function () {
+    return this.dogBreed;
+  }
+
   this.renameDog = function (newName) {
     this.dogName = newName;
   }
+
+  this.changeAge = function (newAge) {
+    this.dogAge = newAge;
+  }
 }
+
+console.log(
+  '\x1b[32mDog v1.0\x1b[0m\n' +
+  'Type \'help\' to display possible commands.\n'
+);
 
 var dogs = [];
 
-var dog = new Dog("Blue", 14, "ACD");
-dogs.push(dog);
+rl.prompt();
+rl.on('line', (userInput) =>
+  {
+    console.log(userInput);
+    var inputData = userInput.trim().split(" ");
+    switch (inputData[0]) {
+      default:
+        console.log('\x1b[31mUnknown command\x1b[0m');
+        rl.prompt();
+        break;
 
-var dog = new Dog("Daisy", 11, "ACD");
-dogs.push(dog);
+      case 'exit':
+        process.exit(0);
 
-for (dog in dogs) {
-  console.log(dogs[dog].getName());
-}
+      case 'help':
+        console.log(
+          'Usage:\n' +
+          '\x1b[32madd <dog name> <dog breed> <dog age>\x1b[0m - Adds dog\n' +
+          '\x1b[32mshow\x1b[0m - Display all dogs\n' +
+          '\x1b[32mshow details <dog name>\x1b[0m - Display detailed information about a dog\n' +
+          '\x1b[32mhelp\x1b[0m - Display this crap\n' +
+          '\x1b[32mexit\x1b[0m - Close program\n'
+        );
+        rl.prompt();
+        break;
 
-for (dog in dogs) {
-  if (dogs[dog].getName() == "Blue") {
-    dogs[dog].renameDog("Putukas");
-  }
-}
+      case 'show':
+        switch (inputData[1]) {
+          default:
+            if (dogs.length == 0) {
+                console.log('No dogs to show');
+              } else {
+                console.log('Your dogs:');
+                for (i in dogs) {
+                  console.log('\x1b[32m' + dogs[i].getName() + '\x1b[0m');
+                }
+            }
+            break;
 
-for (dog in dogs) {
-  console.log(dogs[dog].getName());
-}
+          case 'details':
+            for (i in dogs) {
+              if (dogs[i].getName() == inputData[2]) {
+                console.log(
+                  'Name: \x1b[32m' + dogs[i].getName() + '\x1b[0m\n' +
+                  'Breed: \x1b[32m' + dogs[i].getBreed() + '\x1b[0m\n' +
+                  'Age: \x1b[32m' + dogs[i].getAge() + '\x1b[0m\n'
+                );
+              }
+            }
+            break;
+        }
+        rl.prompt();
+        break;
 
-var averageAge = {
-  dogAge: 0,
-  dogCount: 0,
-  value: function () {
-    if (this.dogCount == 0) {
-      age = 0;
-    } else {
-      age = this.dogAge / this.dogCount;
+      case 'add':
+        var dog = new Dog(inputData[1], inputData[2], inputData[3]);
+        dogs.push(dog);
+        console.log('\x1b[32m' + inputData[1] + '\x1b[0m added');
+        rl.prompt();
+        break;
+
+      case 'age':
+        var averageAge = {
+          dogAge: 0,
+          dogCount: 0,
+          value: function () {
+            if (this.dogCount == 0) {
+              age = 0;
+            } else {
+              age = this.dogAge / this.dogCount;
+            }
+            return age;
+          }
+        }
+        for (i in dogs) {
+          averageAge.dogAge += dogs[i].getAge();
+          averageAge.dogCount++;
+        }
+        console.log('Average age is: ' + averageAge.value());
+        rl.prompt();
+        break;
     }
-    return age;
   }
-}
-
-for (dog in dogs) {
-  averageAge.dogAge += dogs[dog].getAge();
-  averageAge.dogCount++;
-}
-
-console.log(averageAge.value());
+);
